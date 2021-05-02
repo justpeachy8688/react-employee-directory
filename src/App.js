be9a17f1cd8eede00da600d226be37da276f54d6
Taylor Sanders <tsanders3515@gmail.com>
2021-05-02 13:09:13 -0800

    footer and navbar set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 // import './App.css';
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar"
 
 export default function App() {
   return (
     <div>
+      <Navbar />
       <div>Filter goes here</div>
       <div>dataTable goes here</div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <h1 className="title">Employee Directory</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
