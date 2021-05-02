497a9cbba8d72548961743fd5735a5fcd5cf5a81
Taylor Sanders <tsanders3515@gmail.com>
2021-05-02 12:17:58 -0800

    basic set up of files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <div>Filter goes here</div>
+      <div>dataTable goes here</div>
+    </div>
+  );
+}
+
