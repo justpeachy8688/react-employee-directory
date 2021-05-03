85261128350c7f4146b8f1f1a2f732df5beb158e
Taylor Sanders <tsanders3515@gmail.com>
2021-05-02 18:08:36 -0800

    added table and filer components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar"
 
+
 export default function App() {
   return (
     <div>
---
+++ src/components/Table/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+
+function employeeTable() {
+    return (
+        <div>
+            
+
+        </div>
+    );
+}
+
+export default employeeTable;
